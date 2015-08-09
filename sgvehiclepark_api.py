39aadee8a7654396b0dab18312d2b7dd578779ab
Venkatesh JK <venkatesh.jk@gmail.com>
2015-08-10 00:42:56 +0800

    changes

--- sgvehiclepark_api.py
+++ sgvehiclepark_api.py
@@ -82,14 +82,4 @@ class ParkingApi(remote.Service):
 
 
 
-def print_some_times():
-    print time.time()
-    Timer(5, print_time, ()).start()
-    Timer(10, print_time, ()).start()
-    time.sleep(11)  # sleep while time-delay events execute
-    print time.time()
-
-
-
-
 APPLICATION = endpoints.api_server([VehicleUsersApi, ParkingApi])
